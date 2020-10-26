59ac34f3b8bf83f86c49a2273898933182344d0c
HonusDaniel <danielhonus@outlook.cz>
2020-10-26 11:20:01 +0100

    no more album showing, can change in source code first few lines

--- blocks.h
+++ blocks.h
@@ -1,7 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-      { "🎶 ",      "~/dwmblocks/scripts/current_song",         5,   0},
+  { "🎶 ",      "~/dwmblocks/scripts/current_song",         5,   0},
   {"🔊 ", "echo $(pamixer --get-volume)%",       5,      0},
 	{"📟 ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 
