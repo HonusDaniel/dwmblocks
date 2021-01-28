aa61a9a94974db00c3db582e908f9cf80c4ff851
HonusDaniel <danielhonus@outlook.cz>
2021-01-28 14:37:59 +0100

    add_functions

--- blocks.h
+++ blocks.h
@@ -1,10 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+  {"", "~/dwmblocks/scripts/caffeine", 25, 0},
   { "🎶 ",      "~/dwmblocks/scripts/current_song",         5,   0},
+  { "🌎 ",      "surfshark-vpn status | awk /VPN/ | sed 's/Surfshark //'",         30,   0},
   {"🔊 ", "echo $(pamixer --get-volume)%",       5,      0},
-	{"📟 ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
+	{"📟 ", "~/dwmblocks/scripts/memory",	30,		0},
 	{"⌚ ", "date '+%b %d (%a) %I:%M%p'",					5,		0},
 };
 
